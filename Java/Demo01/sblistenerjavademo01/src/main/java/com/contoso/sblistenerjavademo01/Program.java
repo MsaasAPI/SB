f873f35f3927fae6cf545eefc5d25d044039c59b
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-10-03 09:58:12 -0700

    Implement stopwatch.
    Dependency:
          <groupId>org.apache.commons</groupId>
          <artifactId>commons-lang3</artifactId>
          <version>3.8.1</version>

--- Java/Demo01/sblistenerjavademo01/src/main/java/com/contoso/sblistenerjavademo01/Program.java
+++ Java/Demo01/sblistenerjavademo01/src/main/java/com/contoso/sblistenerjavademo01/Program.java
@@ -1,13 +1,30 @@
 package com.contoso.sblistenerjavademo01;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.lang3.time.StopWatch;
+
 public class Program 
 {
+    static StopWatch _stopwatch = new StopWatch();
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        RunStopwatch();
+    }
+
+    /**
+     * Keep running Stopwatch and render the elapsed time, until CTRL + C is pressed
+     * by user.
+     */
+    static void RunStopwatch() {
+        try {
+            _stopwatch.start();
+
+            while (true) {
+                Thread.sleep(10);
+                System.out.print('\r' + _stopwatch.toString().substring(3, 11) + " ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
