d7e5fbe4a3f4e432b31d42abb9517e077104e31f
Jui-wen Liu (Infosys Ltd) <v-juili@microsoft.com>
2018-10-03 11:42:43 -0700

    Implement Initialize().
    Also add empty credential variables. Therefore it will throw error: due to lack of credentials.

--- Java/Demo01/sblistenerjavademo01/src/main/java/com/contoso/sblistenerjavademo01/Program.java
+++ Java/Demo01/sblistenerjavademo01/src/main/java/com/contoso/sblistenerjavademo01/Program.java
@@ -1,16 +1,66 @@
 package com.contoso.sblistenerjavademo01;
 
-import com.microsoft.azure.servicebus.ReceiveMode;
+import com.microsoft.azure.servicebus.*;
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
+
 import org.apache.commons.lang3.time.StopWatch;
 
 public class Program
 {
+    /***** CREDENTIALS *****/
+    static final String CONNECTION_STRING = "";
+    static final String TOPIC_PATH = "";
+    static final String SUBSCRIPTION = "";
+
+    /***** OTHER CONSTANTS & STATIC FIELDS *****/
+    static ISubscriptionClient _subscriptionClient;
     static StopWatch _stopwatch = new StopWatch();
+
+    /***** USER CONFIGURABLE FIELDS *****/
     static ReceiveMode RECEIVE_MODE = ReceiveMode.PEEKLOCK;
 
     public static void main( String[] args )
     {
+        Initialize();
         RunStopwatch();
+    }
+
+    /**
+     * Print Disclaimer, Tip and instantiate Subscription Client.
+     * 
+     * Reference on Peek Lock Expiration:
+     * https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-performance-improvements
+     */
+    static void Initialize() {
+        try {
+            String additionalNote = RECEIVE_MODE == ReceiveMode.PEEKLOCK
+                    ? ". By default, the message lock expires after 60 seconds."
+                : "";
+
+            System.out.println("---------------------------------------------------------------------------------");
+            System.out.println(" Microsoft (R)  Windows Azure SDK                                                ");
+            System.out.println(" Software Development Kit                                                        ");
+            System.out.println("                                                                                 ");
+            System.out.println(" Copyright (c) Microsoft Corporation. All rights reserved.                       ");
+            System.out.println("                                                                                 ");
+            System.out.println(" THIS CODE AND INFORMATION ARE PROVIDED 'AS IS' WITHOUT WARRANTY OF ANY KIND,    ");
+            System.out.println(" EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES");
+            System.out.println(" OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.                     ");
+            System.out.println("---------------------------------------------------------------------------------");
+            System.out.println();
+            System.out.println("==============================================================================");
+            System.out.println("Press any CTRL + C to exit after receiving all the messages");
+            System.out.println("Receive Mode: " + RECEIVE_MODE + additionalNote);
+            System.out.println("==============================================================================");
+            System.out.println();
+
+            // The following credentials shall be obtained from Microsoft tech support.
+            _subscriptionClient = new SubscriptionClient(
+                    new ConnectionStringBuilder(CONNECTION_STRING, TOPIC_PATH + "/subscriptions/" + SUBSCRIPTION),
+                    RECEIVE_MODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
